6bf5cab67fe728767688119b5b91da31895b9558
Chris Coyle <chriscoyle63@gmail.com>
2025-05-29 07:34:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Render!'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running'));
