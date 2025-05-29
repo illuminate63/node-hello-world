17af83cb09cb8ea2a0b854f16a48fb80fdf398aa
Chris Coyle <chris@illuminateadvisory.partners>
2025-05-29 15:24:15 -0400

    Log request to console

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log('request: ' + req.json());
   res.json({
     interventionRisk: "high",
     confidence: 0.87,
